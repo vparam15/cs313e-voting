6f00b85c9714290fb8b3c8d9fb82d599cf6cd8a1
Herbert Gutierrez Rivas <hog74@cs.utexas.edu>
2014-10-16 22:05:16 -0500

    starting code

--- Voting.py
+++ Voting.py
@@ -1,7 +1,16 @@
-def voting_read(r1):
-	r = open(r1, 'r')
-	numcases = r.readline()
-	print(numcases)
+def voting_read():
+	r = open("filename")
+	totalcases = int(r.readline())
+	for case in range (totalcases + 1):
+		r.readline()
+		candidates = int(r.readline())
+		dictionary = {}
+		for i in range (candidates):
+			s = r.readline()
+			dictionary[s[:-1]] = 0 
+		ballots = []
+		while (r.readline is not '/n'): 
+			ballot = r.readline().split()
+			print (ballot)
 
-r1 = firsttest.txt
-voting_read(r1)
+voting_read()
