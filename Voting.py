67b8be1991ea68f1f03737a0b25ae69489982635
Varun Parameswaran <vparam15@gmail.com>
2014-10-22 12:19:38 -0500

    Read function without classes

--- Voting.py
+++ Voting.py
@@ -1,16 +1,21 @@
 def voting_read():
-	r = open("filename")
+	r = open('/v/filer4b/v35q001/vparam/cs313e/projects/cs313e-voting/RunVoting.in', 'r')
 	totalcases = int(r.readline())
 	for case in range (totalcases + 1):
 		r.readline()
 		candidates = int(r.readline())
 		dictionary = {}
-		for i in range (candidates):
+		for i in range (candidates): 
 			s = r.readline()
-			dictionary[s[:-1]] = 0 
+			dictionary[s[:-1]] = 0
+		print(dictionary)
 		ballots = []
-		while (r.readline is not '/n'): 
-			ballot = r.readline().split()
-			print (ballot)
+		while (True):
+			line = r.readline()
+			if not line:
+				break
+			ballot = line.split()
+			print(ballot)
+
 
 voting_read()
