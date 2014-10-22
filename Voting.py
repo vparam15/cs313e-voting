1055a6da28aa51a00a6c8b13f2bde57e6e9aea6f
Varun Parameswaran <vparam15@gmail.com>
2014-10-22 18:56:04 -0500

    Read function now covers all elections; in the process of writing tests to find a winner or a tie in the first run

--- Voting.py
+++ Voting.py
@@ -1,3 +1,4 @@
+import math
 class Candidate:
 	def __init__ (self, name, num):
 		self.name = name
@@ -24,30 +25,47 @@ class Ballot:
 		run += 1
 		return x
 
+def find_winner (clist, majority):
+	for c in clist:
+		if c.count >= majority:
+			print(c.name)
+			break
+
+def find_tie (clist, tie_num):
+	all_tied = True
+	for c in clist:
+		if c.count != tie_num:
+			all_tied == False
+	if all_tied == True:
+		print ("No winner")
+
 def voting_read():
 	r = open('/v/filer4b/v35q001/vparam/cs313e/projects/cs313e-voting/RunVoting.in', 'r')
-	# while (True):
-	# 	line = r.readline()
-	# 	if not line:
-	# 		break
-	# 	totalcases = int(line)
-	# 	for case in range (totalcases + 1):
-	totcases = int(r.readline())
-	r.readline()
-	totcandidates = int(r.readline())
-	clist = []
-	for i in range (totcandidates): 
-		clist.append(Candidate(r.readline(), i + 1))
 	while (True):
 		line = r.readline()
-		if (line == ''):
+		if not line:
 			break
-		ballot = Ballot(line.split())
-		choice1 = ballot.getchoice()
-		for c in clist:
-			if (choice1 == c.num):
-				c.increment(ballot)
-	for c in clist:
-		print (c.name, c.num, c.count, c.ballots)
+		totelections = int(line)
+		r.readline()
+		for election in range (totelections):
+			numBallots = -1
+			totcandidates = int(r.readline())
+			clist = []
+			for i in range (totcandidates): 
+				clist.append(Candidate(r.readline(), i + 1))
+			while (True):
+				numBallots += 1
+				line = r.readline()
+				if len(line) <= 1:
+					break
+				ballot = Ballot(line.split())
+				choice1 = ballot.getchoice()
+				for c in clist:
+					if (choice1 == c.num):
+						c.increment(ballot)
+			majority = math.ceil(numBallots / 2)
+			tie_num = numBallots/totcandidates
+			find_winner (clist, majority)
+			find_tie (clist, tie_num)
 
 voting_read()
