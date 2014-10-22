49cf5c819be81b232b5de865a2c7513fa545acf3
Varun Parameswaran <vparam15@gmail.com>
2014-10-22 13:40:17 -0500

    Added classes; still need to put ballots into candidates' buckets

--- Voting.py
+++ Voting.py
@@ -1,21 +1,50 @@
+class Candidate:
+	def __init__ (self, name, num):
+		self.name = name
+		self.num = num
+		self.count = 0
+		self.ballots = []
+
+	def getname (self):
+		return self.name
+
+	def getcount (self):
+		return self.count
+
+	def increment (self, ballot):
+		self.count += 1
+		self.ballots.append (ballot)
+
+class Ballot:
+	def __init__ (self, choices):
+		self.choices = choices
+
+	def getchoice (self, run = 0):
+		x = self.choices[run]
+		run += 1
+		return x
+
 def voting_read():
 	r = open('/v/filer4b/v35q001/vparam/cs313e/projects/cs313e-voting/RunVoting.in', 'r')
-	totalcases = int(r.readline())
-	for case in range (totalcases + 1):
-		r.readline()
-		candidates = int(r.readline())
-		dictionary = {}
-		for i in range (candidates): 
-			s = r.readline()
-			dictionary[s[:-1]] = 0
-		print(dictionary)
-		ballots = []
-		while (True):
-			line = r.readline()
-			if not line:
-				break
-			ballot = line.split()
-			print(ballot)
-
+	# while (True):
+	# 	line = r.readline()
+	# 	if not line:
+	# 		break
+	# 	totalcases = int(line)
+	# 	for case in range (totalcases + 1):
+	totcases = int(r.readline())
+	r.readline()
+	totcandidates = int(r.readline())
+	clist = []
+	for i in range (totcandidates): 
+		clist.append(Candidate(r.readline(), i + 1))
+	# for c in clist:
+	# 	print (c.name, c.num)
+	while (True):
+		line = r.readline()
+		if (line == ''):
+			break
+		ballot = Ballot(line.split())
+		
 
 voting_read()
