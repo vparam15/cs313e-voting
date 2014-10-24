63392f7afc0330be346bf8d5d352618a56f8191c
Varun Parameswaran <vparam15@gmail.com>
2014-10-24 18:22:45 -0500

    Created Voting.html and pushed all files to public repo

--- Voting.py
+++ Voting.py
@@ -1,32 +1,31 @@
-import math
-
-class Candidate:
-	def __init__ (self, name, num):
+class Candidate: 
+	def __init__ (self, name, num): # initialize Candidate class with name, number, count of ballots, list of ballots, and boolean describing whether they are still running or not
 		self.name = name
 		self.num = num
 		self.count = 0
 		self.ballots = []
 		self.running = True
 
-	def getinfo (self):
+	def getinfo (self): # return all Candidate info
 		return (self.name, self.num, self.count, self.ballots, self.running)
 
-	def increment (self, b):
+	def increment (self, b): # Increase the ballot count by 1 and add a ballot to the ballot list
 		self.count += 1
 		self.ballots.append (b)
 
 class Ballot:
-	def __init__ (self, choices):
+	def __init__ (self, choices): # Initialize Ballot class with list of choices and a marker
 		self.choices = choices
 		self.marker = 0
 
-	def getchoice (self):
+	def getchoice (self): # return the ballot choice pointed to by the marker
 		x = int(self.choices[self.marker])
 		return x
 
-	def getinfo (self):
+	def getinfo (self): # Return all Ballot info
 		return (self.choices, self.marker)
 
+# Finds a clear winner (greater than 50% of votes) and if found, writes to output
 def find_winner (clist, numBallots, w):
 	outfile = (w)
 	tie = numBallots // 2
@@ -45,6 +44,7 @@ def find_winner (clist, numBallots, w):
 		return True
 	return False
 
+# Finds an "all-way" tie and if found, writes to output
 def find_tie (clist, numBallots, w):
 	outfile = (w)
 	tie = numBallots / len(clist)
@@ -63,6 +63,7 @@ def find_tie (clist, numBallots, w):
 		return True
 	return False
 
+# Finds all losers (less than the maximum number of votes earned by candidate(s)) and returns them in a list
 def find_losers (clist, numBallots):
 	max_count = 1
 	loser_list = []
@@ -73,6 +74,7 @@ def find_losers (clist, numBallots):
 			loser_list.append (c)
 	return loser_list
 
+# Removes all losers from the overall candidate list
 def empty_losers (clist, loser_list):
 	removelist = []
 	for c in clist:
@@ -85,6 +87,7 @@ def empty_losers (clist, loser_list):
 		reassign (c, clist)
 		clist.remove(c)
 
+# Reassigns the ballots of the losers, based on subsequent choices, to another candidate
 def reassign (loser, clist):
 	found = False
 	new_c = None
@@ -100,7 +103,9 @@ def reassign (loser, clist):
 					break
 		new_c.increment(ballot)
 
+# Reads input, populates candidate list with first ballots, and runs tests one-by-one
 def voting_solve (r, w):
+	# Reads input by election, then by candidate, then by ballot
 	while (True):
 		line = r.readline()
 		if not line:
@@ -113,6 +118,8 @@ def voting_solve (r, w):
 			clist = []
 			for i in range (totcandidates): 
 				clist.append(Candidate(r.readline(), i + 1))
+			
+			# Populates candidate list with first ballots of each candidate
 			while (True):
 				numBallots += 1
 				line = r.readline()
@@ -124,6 +131,7 @@ def voting_solve (r, w):
 					if (choice1 == c.num):
 						c.increment(ballot)
 
+			# Runs tests for clear winner, clear tie, and reassignment of losers one-by-one
 			while True:
 				found_winner = find_winner (clist, numBallots, w)
 				if found_winner:
